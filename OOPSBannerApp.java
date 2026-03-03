968891c3d9b2a1da0be8872bb11e50dc584d2e8b
Adithya Uday <au1329@srmist.edu.in>
2026-03-03 13:43:23 +0400

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,16 @@
+/**
+ * OOPSBannerApp
+ * 
+ * UC1: Print OOPS to Console
+ * 
+ * @author Adithya
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+    
+}
